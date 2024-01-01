f6cecef17be83b39c6e4596a03d591d9e122ae3f
Your Name <you@example.com>
2024-01-01 17:42:41 +0100

    inicio proyecto

---
+++ src/config/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Montserrat_Alternates } from "next/font/google";
+
+export const fontBody = Inter({subsets: ['latin']})
+export const titleFont = Montserrat_Alternates({
+    subsets: ['latin'],
+    weight: ['500', '700']
+})
+
